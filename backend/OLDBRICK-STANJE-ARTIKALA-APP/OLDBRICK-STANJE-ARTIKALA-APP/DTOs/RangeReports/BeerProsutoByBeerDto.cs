c6e62f80f4a25ab32718e6aee6d9877b53d8d021
BursacAleksandar99 <aleksandarbursac150699@gmail.com>
2025-12-24 14:55:54 +0100

    Created new Dtos for next measuring

---
+++ backend/OLDBRICK-STANJE-ARTIKALA-APP/OLDBRICK-STANJE-ARTIKALA-APP/DTOs/RangeReports/BeerProsutoByBeerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OLDBRICK_STANJE_ARTIKALA_APP.DTOs.RangeReports
+{
+    public class BeerProsutoByBeerDto
+    {
+
+        public int BeerId { get; set; }
+        public string BeerName { get; set; } = string.Empty;
+        public float TotalAppProsuto { get; set; }
+    }
+}
---
+++ backend/OLDBRICK-STANJE-ARTIKALA-APP/OLDBRICK-STANJE-ARTIKALA-APP/DTOs/RangeReports/BeerProsutoRangeReportDto.cs
@@ -0,0 +1,14 @@
+﻿using OLDBRICK_STANJE_ARTIKALA_APP.DTOs.RangeReports;
+
+namespace OLDBRICK_STANJE_ARTIKALA_APP.DTOs.RangeReports
+{
+    public class BeerProsutoRangeReportDto
+    {
+        public DateOnly From { get; set; }
+        public DateOnly To { get; set; }
+        public float TotalMeasuredProsuto { get; set; } 
+        public float TotalAppProsuto { get; set; }
+        public float TotalDifference => TotalMeasuredProsuto - TotalAppProsuto;
+        public List<BeerProsutoByBeerDto> ByBeer { get; set; } = new();
+    }
+}
